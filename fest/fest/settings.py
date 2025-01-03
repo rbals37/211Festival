3bac37ea578912e3236ee9c0c6ab206f2506464d
swoiun <swoiun@icloud.com>
2025-01-03 13:44:18 +0900

    settigs.py 좀 수정

--- fest/fest/settings.py
+++ fest/fest/settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
